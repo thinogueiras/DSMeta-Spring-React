f41394285ff3f826d19cf8fce0f85832f03752c6
thinogueiras <thiagonogueira@hotmail.com.br>
2022-11-18 08:18:16 -0300

    Lombok

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -8,10 +8,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
-@Table(name= "tb_sales")
+@Table(name = "tb_sales")
 public class Sale {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,56 +29,4 @@ public class Sale {
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
-	
-	public Sale() {
-		
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getSellerName() {
-		return sellerName;
-	}
-
-	public void setSellerName(String sellerName) {
-		this.sellerName = sellerName;
-	}
-
-	public Integer getVisited() {
-		return visited;
-	}
-
-	public void setVisited(Integer visited) {
-		this.visited = visited;
-	}
-
-	public Integer getDeals() {
-		return deals;
-	}
-
-	public void setDeals(Integer deals) {
-		this.deals = deals;
-	}
-
-	public Double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
 }
